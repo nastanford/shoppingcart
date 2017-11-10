f9e60316c6e26c359c3856eb0ae142b1fc9080a6
Nathan Stanford Sr <nathan.stanford@gmail.com>
2017-11-10 16:49:22 -0500

    Moved the tests out of the app folder.

---
+++ test/next/second_spec.js
@@ -0,0 +1,9 @@
+assert = require("assert");
+
+describe("2 A feature", function() {
+  describe("2 A scenario", function() {
+    it("2 does something expected", function() {
+      assert(true);
+    });
+  });
+});
